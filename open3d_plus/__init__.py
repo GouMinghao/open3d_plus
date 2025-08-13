17b05360ec8b832a28f8879c04191b086336be3c
GouMinghao <gmh2015@163.com>
2025-08-13 23:15:34 +0800

    ENH: update to 0.2.0

--- open3d_plus/__init__.py
+++ open3d_plus/__init__.py
@@ -1,5 +1,5 @@
 from .geometry import array2pcd, pcd2array, merge_pcds, generate_scene_pointcloud
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = ["array2pcd", "pcd2array", "merge_pcds", "generate_scene_pointcloud", "create_axis"]
